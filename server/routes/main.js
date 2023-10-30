ac40044ea4d25cb84a8c4e975261d1afe792dc76
Serdar Mor <serdar34mor@gmail.com>
2023-10-30 23:12:25 +0300

    updated

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
-//Routes
-router.get('', (req,res) => {
-    const locals = {
-        title: "NodeJs Blog",
-        description: "Simple Blog"
+/**
+ * GET /
+ * HOME
+ */
+router.get('', async (req,res) => {     
+
+    try {
+        const locals = {
+            title: "NodeJs Blog",
+            description: "Simple Blog"
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([{$sort : {createdAt: -1}}])
+        .skip(perPage*page-perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.count();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count/perPage); 
+
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+
+    } catch (error) {
+        console.log(error);
     }
-    res.render('index', {locals});
+    
 });
+
 
 router.get('/about',(req,res) => {
     res.render('about');
