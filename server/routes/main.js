c84e031d1c4cc8cd16a65ea6061bcc80855bdede
Serdar Mor <serdar34mor@gmail.com>
2023-10-31 00:32:52 +0300

    updated

--- server/routes/main.js
+++ server/routes/main.js
@@ -39,6 +39,64 @@ router.get('', async (req,res) => {
     
 });
 
+/**
+ * GET /
+ * Post :id
+ */
+
+router.get('/post/:id', async(req,res) => {
+
+    try {
+
+        let slug = req.params.id;
+
+        const data = await Post.findById({_id: slug});
+
+        const locals = {
+            title: data.title,
+            description: data.body.slice(0,50).concat('...')
+        }
+
+        res.render('post',{locals,data});
+
+
+    } catch (error) {
+        console.log(error);
+    }
+
+});
+
+
+/**
+ * POST /
+ * Post - searchTerm
+ */
+
+router.post('/search', async (req,res) => {
+
+    try {
+
+        const locals = {
+            title: "Search",
+            description: "Simple Blog"
+        }
+
+        let searchTerm = req.body.searchTerm;
+        const searchNoSpecialChar = searchTerm.replace(/[^a-zA-Z0-9]/g,"");
+
+        const data = await Post.find({
+            $or: [
+                {title: { $regex: new RegExp(searchNoSpecialChar,'i')}},
+                {body: { $regex: new RegExp(searchNoSpecialChar,'i')}}
+            ]
+        });
+
+        res.render('search', {locals,data});
+    } catch (error) {
+        console.log(error);
+    }
+});
+
 
 router.get('/about',(req,res) => {
     res.render('about');
